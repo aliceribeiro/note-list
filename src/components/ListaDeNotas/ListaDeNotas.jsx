923db67da0bda03b7b47af1a13c800b60a7d5060
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-09-16 21:37:09 -0300

    feat: :sparkles: cria os componentes de formulario, card e lista de notas

---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CardNota from '../CardNota';
+import './style.css';
+
+class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul className="lista-notas">
+        {Array.of('Trabalho', 'Trabalho', 'Estudos').map((categoria, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
