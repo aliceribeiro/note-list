bb22f8d5a65dd2a6cca022e4d82dad2745ac424b
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2021-10-10 23:55:36 -0300

    feat: :sparkles: implementa a opcaoo de apagar nota

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './style.css';
+import { ReactComponent as DeleteIcon } from '../../assets/icons/delete.svg';
 
 class CardNota extends Component {
+  apagar() {
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteIcon onClick={this.apagar.bind(this)} />
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
